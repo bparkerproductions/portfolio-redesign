90d1d6dd8c92fe1133b20ffabd05e77f8fb639ee
Scott Reimels <bparkerproductions@gmail.com>
2019-01-19 16:32:29 -0900

    testimonial desktop

--- web/app/themes/bp-portfolio/resources/views/partials/home/testimonials.blade.php
+++ web/app/themes/bp-portfolio/resources/views/partials/home/testimonials.blade.php
@@ -1,22 +1,52 @@
 <section class="testimonials column-center spacer">
   <div class="inner-container">
 
-    <div class="tesimonial-container">
+    <div class="testimonial-container">
+
+    {{-- Previous Toggle --}}
+      <div class="previous">
+        <i class="fas fa-chevron-left"></i>
+      </div>
+
+      {{-- Testimonials --}}
       @foreach($testimonials as $testimonial)
         <div class="testimonial">
           <div class="content">
-            {!! $testimonial['text']!!}
+            <div class="left-quote-container">
+              <i class="fas fa-quote-left"></i>
+            </div>
+            <div class="inner-content">
+              <p>
+                {!! $testimonial['text']!!}
+              </p>
+            </div>
+            <div class="right-quote-container">
+              <i class="fas fa-quote-right"></i>
+            </div>
           </div>
           <div class="client-info">
-            <p>
-              {{$testimonial['name']}}
+            <p class="client-name">
+              <i class="fas fa-user"></i>
+              <span class="text">
+                {{$testimonial['name']}}
+              </span>
             </p>
-            <a href="{{$testimonial['company']}}">
-              Website
+
+            <a class="client-company" 
+            href="{{$testimonial['company']}}">
+              <i class="fas fa-briefcase"></i>
+              <span class="link">
+                Website
+              </span>
             </a>
           </div>
         </div>
       @endforeach
+
+      {{-- Next Toggle --}}
+      <div class="next">
+        <i class="fas fa-chevron-right"></i>
+      </div>
     </div>
   </div>
 </section>
