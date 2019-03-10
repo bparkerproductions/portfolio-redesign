5008a93a768d61eba6992849d36330d3e01366cf
Brandon <bparkerproductions@gmail.com>
2019-03-10 10:29:25 -0800

    footer

--- web/app/themes/bp-portfolio/app/Controllers/Partials/GlobalOptions.php
+++ web/app/themes/bp-portfolio/app/Controllers/Partials/GlobalOptions.php
@@ -4,14 +4,22 @@ namespace App\Controllers\Partials;
 
 trait GlobalOptions {
   public function socialMedia() {
-    return get_field('social_media', 'option');
+    return get_field("social_media", "option");
   }
 
   public function ctaHeader() {
-    return get_field('cta_header', 'option');
+    return get_field("cta_header", "option");
   }
 
   public function ctaEmailLink() {
-    return get_field('cta_email_link', 'option');
+    return get_field("cta_email_link", "option");
+  }
+
+  public function footerLists() {
+    return get_field("footer_lists", "option");
+  }
+
+  public function copyrightText() {
+    return get_field("copyright_text", "option");
   }
 }
--- web/app/themes/bp-portfolio/resources/views/partials/footer.blade.php
+++ web/app/themes/bp-portfolio/resources/views/partials/footer.blade.php
@@ -1,5 +1,29 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+<footer class="footer column-center">
+  <div class="inner-container">
+
+    {{-- Footer Top --}}
+    <div class="list-container footer-top spacer">
+      @foreach($footer_lists as $list)
+        <div class="footer-col">
+          @if($list['header'])
+            <h6 class="white">{{$list['header']}}</h6>
+          @endif
+          <ul class="list">
+            @foreach($list['link'] as $link)
+              <li class="white">
+                <a href="{{$link['item']['url']}}">
+                  {{$link['item']['title']}}
+                </a>
+              </li>
+            @endforeach
+          </ul>
+        </div>
+      @endforeach
+    </div>
+
+    {{-- Footer Bottom --}}
+    <div class="footer-bottom">
+      <p>{{$copyright_text}}</p>
+    </div>
   </div>
 </footer>
