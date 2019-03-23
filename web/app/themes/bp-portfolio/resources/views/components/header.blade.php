ae1555941ffb8c893d84de17b2cb898f1a14343c
Brandon <bparkerproductions@gmail.com>
2019-03-22 22:44:16 -0800

    responsive social share

--- web/app/themes/bp-portfolio/resources/views/components/header.blade.php
+++ web/app/themes/bp-portfolio/resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
 <section class="archive-header column-center">
-    <div class="inner-container">
-      <h1>{!!APP::title()!!}</h1>
-      <div class="subtitle-container">
-        {!! $slot !!}
-      </div>
+  <div class="inner-container">
+    <h1>{!!APP::title()!!}</h1>
+    <div class="subtitle-container">
+      {!! $slot !!}
     </div>
-  </section>
+  </div>
+</section>
