a2d9cdcc0e67c3ee945edf7c51f815f13914f0a6
Brandon <bparkerproductions@gmail.com>
2019-04-14 19:00:10 -0800

    about section tech redesign, added labels to icons

--- web/app/themes/bp-portfolio/resources/views/partials/home/about.blade.php
+++ web/app/themes/bp-portfolio/resources/views/partials/home/about.blade.php
@@ -1,32 +1,37 @@
 <section id="about-container" class="column-center spacer about-section">
   <div class="inner-container">
-    <div class="content-container">
-      <h1 class="blue">
-        {{$about_title}}
-      </h1>
-      <p class="para">
-        {!!$about_desc!!}
-      </p>
+    <div class="top-row">
+      <div class="content-container">
+        <h1 class="blue">
+          {{$about_title}}
+        </h1>
+        <p class="para">
+          {!!$about_desc!!}
+        </p>
 
-      <div class="button-container">
-        <a href="{{$about_btn_link}}">
-          <button class="button black">
-            {{$about_btn_text}}
-            <i class="fas fa-caret-right"></i>
-          </button>
-        </a>
+        <div class="button-container">
+          <a href="{{$about_btn_link}}">
+            <button class="button black">
+              {{$about_btn_text}}
+              <i class="fas fa-caret-right"></i>
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div class="technologies-container">
+        <h2 class="blue tech-header">{{$tech_header}}</h2>
+        <div class="technologies">
+          @foreach($technologies as $tech)
+            <div class="tech-col">
+              <i class="{{$tech['icon']}}"></i>
+              <span class="tech-label">{{$tech['label']}}</span>
+            </div>
+          @endforeach
+        </div>
       </div>
     </div>
 
-    <div class="technologies-container">
-      <h2 class="blue">
-        {{$tech_header}}
-      </h2>
-      <div class="technologies">
-        @foreach($technologies as $tech)
-          <i class="{{$tech['icon']}}"></i>
-        @endforeach
-      </div>
-    </div>
+
   </div>
 </section>
