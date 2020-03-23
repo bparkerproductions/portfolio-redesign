1c43891e777499ae03c5b635f25eeb353fcc8f85
Brandon <bparkerproductions@gmail.com>
2020-03-22 17:31:21 -0700

    fix project single hero links

--- web/app/themes/bp-portfolio/resources/views/components/header.blade.php
+++ web/app/themes/bp-portfolio/resources/views/components/header.blade.php
@@ -2,11 +2,6 @@
   <div class="inner-container">
     <h2 class="header">{!!APP::title()!!}</h2>
     <div class="subtitle-container">
-      @if($icon_class)
-        <div class="icon-container">
-          <i class="{{$icon_class}}"></i>
-        </div>
-      @endif
       {!! $slot !!}
     </div>
   </div>
--- web/app/themes/bp-portfolio/resources/views/single.blade.php
+++ web/app/themes/bp-portfolio/resources/views/single.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @component('components.header', [
-      'icon_class' => Single::getIconClass()
-    ])
+    @component('components.header')
       @if(get_post_type() == 'projects')
-        <a href="{{$project_link}}">
-          See {!! $project_title !!} Live
-        </a>
+        @if($project_link)
+          <a href="{{$project_link}}">
+            See {!! $project_title !!} Live
+          </a>
+        @endif
       @endif
       @if(get_post_type() == 'demos')
         <p>{{$demo_description}}</p>
