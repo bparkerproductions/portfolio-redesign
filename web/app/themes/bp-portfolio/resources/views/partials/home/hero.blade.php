74d1bec81714c78026b82fe4d8d704bff9c9a959
Brandon <bparkerproductions@gmail.com>
2020-03-25 11:42:11 -0700

    home hero slight redesign

--- web/app/themes/bp-portfolio/resources/views/partials/home/hero.blade.php
+++ web/app/themes/bp-portfolio/resources/views/partials/home/hero.blade.php
@@ -2,13 +2,30 @@
   <div class="layout-two-columns">
     <div class="left-column col">
       <h2 class="title">{{ $home_hero['section_1_header'] }}</h2>
-      <p class="description">{{ $home_hero['text_section_1'] }}</p>
-      <a href="{{ $portfolio_link }}" class="portfolio-link">See Portfolio </a>
+      @if($home_hero['text_section_1'])
+        <p class="description">{{ $home_hero['text_section_1'] }}</p>
+      @endif
+      <div class="button-container mt-small">
+        <a href="{{ $portfolio_link }}" class="portfolio-link">
+          <button class="button border white">
+            See Portfolio
+          </button>
+        </a>
+      </div>
     </div>
     <div class="right-column col">
       <h2 class="title">{{ $home_hero['section_2_header'] }}</h2>
-      <p class="description">{{ $home_hero['text_section_2'] }}</p>
-      <a href="#" class="scroll-to-about">Keep scrolling!</a>
+      @if($home_hero['text_section_2'])
+        <p class="description">{{ $home_hero['text_section_2'] }}</p>
+      @endif
+      <div class="button-container mt-small">
+        <a href="{{ $portfolio_link }}" class="portfolio-link">
+          <button class="button border white rotate">
+            Learn More!
+            <i class="fa fa-caret-right"></i>
+          </button>
+        </a>
+      </div>
     </div>
   </div>
 </section>
